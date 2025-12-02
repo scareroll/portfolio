a14623c2025e5a9414e8f6c644f80c93754e7820
scareroll <zvasdj.7666@gmail.com>
2025-12-02 18:26:44 +0300

    scareroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://scareroll.vercel.app/og.png",
-        width: 681,
-        height: 241,
+        width: 1920,
+        height: 1080,
       },
     ],
     locale: "en-US",
